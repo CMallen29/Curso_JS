0a2f417b9146fb0c5922b5732bc4723a661bc330
MallenDev <mallensound@gmail.com>
2023-08-24 13:58:34 +0200

    Creacion introduccion

---
+++ 01 - Introduccion/codigo.js
@@ -0,0 +1,14 @@
+//Datos primitivos y variables
+
+string = "cadena de texto"
+number = 19
+boleano = true
+
+var numero = 15;
+let numero = 14;
+const IVA = 21;
+
+//Tipos de datos UNDEFINED y NULL
+
+
+
