5a0589dee7db8893e2034a46c266671f5fa0336d
MallenDev <mallensound@gmail.com>
2023-09-05 16:33:50 +0200

    Enunciado ejercicio 2

---
+++ 02 - Arrays_Bucles_Funciones/Ejercicio2/codigo.js
@@ -0,0 +1,13 @@
+/*
+
+Pepe empieza la universidad del ciclo de desarrollo de sotware. 
+En su curso hay 19 alumnos, pero hay un error en el sitema que no permite registrar datos en 30 dias.
+
+    -Crear una solucion que permita registrar alumnos presentes (P) y ausentes (A) en la clase.
+    -Pasados 30 dias, mostrar situacion final de los alumnos (No el total de presentes/ausentes).
+    -Si alguin tiene mas del 10% de faltas, estará suspenso.
+
+*/
+
+
+
