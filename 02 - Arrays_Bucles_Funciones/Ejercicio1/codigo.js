85e2a7ba9d0e3d999881ce815236136efad00b2d
MallenDev <mallensound@gmail.com>
2023-09-05 16:28:11 +0200

    Resuelto ejercicio1

--- 02 - Arrays_Bucles_Funciones/Ejercicio1/codigo.js
+++ 02 - Arrays_Bucles_Funciones/Ejercicio1/codigo.js
@@ -7,4 +7,31 @@ Pepe celebra una fiesta porque le ha tocado la loteria, pero al local no puede e
 
 */
 
+let entradaGratis = false;
 
+const validarCliente = (hora) => {
+
+    let edad = prompt("Cual es tu edad?");
+
+    if (edad > 18){
+        if (hora >= 2 && hora < 8 && entradaGratis != true){
+            alert(`Son las ${hora}. Puedes pasar grartis !!`);
+            entradaGratis = true;
+        }else{
+            alert(`Son las ${hora}. Puedes pasar pagando la entrada.`);
+        }
+
+    }else{
+        alert("Solo pueden pasar los mayores de edad.");
+    }
+
+}
+
+
+validarCliente(23);
+validarCliente(24);
+validarCliente(1);
+validarCliente(2);
+validarCliente(3);
+validarCliente(4);
+validarCliente(8);
