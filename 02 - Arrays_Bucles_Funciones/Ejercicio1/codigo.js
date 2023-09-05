7978ce868da34cfbf44c4d81f1119a59e422c6aa
MallenDev <mallensound@gmail.com>
2023-09-05 16:10:42 +0200

    ejercicios parte 2

---
+++ 02 - Arrays_Bucles_Funciones/Ejercicio1/codigo.js
@@ -0,0 +1,10 @@
+/*
+
+Pepe celebra una fiesta porque le ha tocado la loteria, pero al local no puede entrar cualquiera:
+
+    -Solo pueden pasar mayores de edad
+    -La primera persona que entre despues de las 2 no paga.
+
+*/
+
+
